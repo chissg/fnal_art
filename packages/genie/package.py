5b21e3599c84923469bc8962a4738f264aa00a31
Nathaniel Rowe <nathanielerowe@polaris-login-04.hsn.cm.polaris.alcf.anl.gov>
2024-08-09 20:33:46 +0000

    libxpm temp fix

--- packages/genie/package.py
+++ packages/genie/package.py
@@ -71,11 +71,12 @@ class Genie(AutotoolsPackage):
     depends_on("lhapdf" , when="+lhapdf")
 
     # Issues caused by default root cxxstd being 11
-    depends_on("root @6.24.08: +pythia6 cxxstd=11", when="cxxstd=11")
-    depends_on("root @6.24.08: +pythia6 cxxstd=14", when="cxxstd=14")
-    depends_on("root @6.24.08: +pythia6 cxxstd=17", when="cxxstd=17")
-    depends_on("root @6.24.08: +pythia6 cxxstd=17", when="cxxstd=default")
-    depends_on("root @6.24.08: +pythia6 cxxstd=20", when="cxxstd=20")
+    # Separate issue caused by libxmp from +x requirement
+    depends_on("root @6.24.08: +pythia6 ~x cxxstd=11", when="cxxstd=11")
+    depends_on("root @6.24.08: +pythia6 ~x cxxstd=14", when="cxxstd=14")
+    depends_on("root @6.24.08: +pythia6 ~x cxxstd=17", when="cxxstd=17")
+    depends_on("root @6.24.08: +pythia6 ~x cxxstd=17", when="cxxstd=default")
+    depends_on("root @6.24.08: +pythia6 ~x cxxstd=20", when="cxxstd=20")
     depends_on("pythia6+root")
     depends_on("libxml2")
     depends_on("log4cpp")
