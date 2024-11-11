122e04ddccc1d50ac320024c8405d7914c7c0c29
Chris Green <greenc@fnal.gov>
2024-11-11 16:11:10 -0600

    [cetmodules] New version 3.27.00

--- packages/cetmodules/package.py
+++ packages/cetmodules/package.py
@@ -21,6 +21,7 @@ class Cetmodules(CMakePackage):
 
     version("develop", branch="develop", get_full_repo=True)
 
+    version("3.27.00", sha256="6926fd8df351713bce0382ab57760d398437687e24da40bbd1306d6b17d86661")
     version("3.26.00", sha256="0acf1a916cb378dba819dd954de1e5279db5053658827844886006466cec3dc8")
     version("3.25.00", sha256="9574d9f8e5757d79c11d6fb3b95c3227aa0028960733715c3e6b6b25a51ff5c5")
     version("3.24.01", sha256="6ef8b522b02298a787a318cda898deedc250cdd336dc3168fad934ef607b5916")
